3472844325102a5e8fd9ec08e33758c6aa8f176a
ethnwr <daniil.gawrilov07@gmail.com>
2025-05-02 14:52:50 +0900

    Create potok3.cpp

---
+++ potok3.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <thread>
+#include <mutex>
+#include <condition_variable>
+
+using namespace std;
+
+mutex mtx;
+condition_variable cv;
+bool ready = false;
+
+void workerThread() {
+    unique_lock<mutex> lock(mtx);
+    cout << "Рабочий поток: Я ЖДУ...\n" << endl;
+
+    cv.wait(lock, [] { return ready; });
+
+    cout << "Рабочий поток: Чиназес. Продолжаю работу." << endl;
+    for (int i = 0; i < 5; ++i) {
+        cout << "Рабочий поток:обрабатываю данные. " << i << endl;
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    thread worker(workerThread);
+
+    cout << "Главный поток: Я СОЮИРАЮСЬ..." << endl;
+    this_thread::sleep_for(chrono::seconds(2));
+
+    {
+        lock_guard<mutex> lock(mtx); 
+        ready = true;
+        cout << "Главный поток: Чиназес отправля. сигнал" << endl;
+    }
+
+    cv.notify_one(); 
+
+    worker.join();
+
+    cout << "Главный поток: Программа завершена." << endl;
+
+    return 0;
+}

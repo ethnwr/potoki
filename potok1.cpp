750c71fe2d5d2f5e603d300bfd2eca286ab4b3cc
ethnwr <daniil.gawrilov07@gmail.com>
2025-05-02 14:52:06 +0900

    Create potok1.cpp

---
+++ potok1.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <thread>
+
+using namespace std;
+
+void vvivodCisel() {
+    for (int i = 1; i <= 100; ++i) {
+        cout << "Число: " << i << endl;
+    }
+}
+
+void vvivodBukv() {
+    for (char c = 'A'; c <= 'Z'; ++c) {
+        cout << "Буква: " << c << endl;
+    }
+}
+
+int main() {
+    setlocale(LC_ALL, "ru");
+    thread cisleniiThread(vvivodCisel);
+    thread bukviThread(vvivodBukv);
+
+    cisleniiThread.join();
+    bukviThread.join();
+
+    cout << "Оба потока завершили работу." << endl;
+
+    return 0;
+}
